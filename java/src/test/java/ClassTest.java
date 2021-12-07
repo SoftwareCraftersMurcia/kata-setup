a0d16d52653ca8f9fe61bdedcf6e5d77e7ab49c1
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-12-07 19:54:49 +0100

    Update and rename FizzBuzzTest.java to ClassTest.java

--- java/src/test/java/ClassTest.java
+++ java/src/test/java/ClassTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class FizzBuzzTest {
+public class ClassTest {
 
 	@Test
 	public void renameMe() {
