4196a7207efbffa0c9a015e52788c89881f1da89
katarn <katarn@katarn.es>
2023-03-24 18:11:17 +0100

    Update base bootstraping to a more standard structure

---
+++ typescript/src/Example.ts
@@ -0,0 +1,5 @@
+export class Example {
+    static method(): boolean {
+        return true
+    }
+}
--- typescript/tests/unit/kata-example.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('kata-setup', () => {
-    it('example test', () => {
-        expect(true).toBeTruthy();
-    });
-});
