0ea302b7ecb616160d2ebd589c82833ba0c84209
antonio-gg-dev <dev@antonio.gg>
2024-05-17 22:56:04 +0200

    refactor tests rock beats paper

--- typescript/tests/Example.spec.ts
+++ typescript/tests/Example.spec.ts
@@ -24,24 +24,14 @@ describe('Rock Paper Scissors', () => {
     ).toBe(winner)
   });
 
-  describe('Player Two wins', () => {
-    it('given that the second player uses paper beats rock', () => {
-      expect(
-        game.plays(Movements.ROCK, Movements.PAPER)
-      )
-        .toBe('Player_2')
-    })
+  it.each([
+    [Movements.ROCK, Movements.PAPER, 'Player_2'],
+    [Movements.PAPER, Movements.ROCK, 'Player_1']
+  ])('rock should beat paper', (movementPlayerOne: Movements, movementPlayerTwo: Movements, winner: string) => {
+    expect(
+      game.plays(movementPlayerOne, movementPlayerTwo)
+    ).toBe(winner)
   });
-
-  describe('Player One wins', ()=> {
-    it('given that the first player uses paper beats rock', () => {
-      expect(
-        game.plays(Movements.PAPER, Movements.ROCK)
-      )
-        .toBe('Player_1')
-    })
-    
-  })
 });
 
 
