a321dba71129b7bd1ff4550e9f9fabbd6234a61f
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2024-05-17 23:17:03 +0200

    winningMovements

--- typescript/src/RockPaperScissors.ts
+++ typescript/src/RockPaperScissors.ts
@@ -6,24 +6,30 @@ export enum Movements {
 }
 
 export class RockPaperScissors {
-  plays(playerOneMovement: Movements, playerTwoMovement: Movements) {
-    if (playerOneMovement === Movements.PAPER && playerTwoMovement === Movements.SCISSORS) {
-        return 'Player_2'
-    }
-    if (playerOneMovement === Movements.PAPER && playerTwoMovement !== Movements.SCISSORS) {
-      return 'Player_1'
-    }
+    plays(playerOneMovement: Movements, playerTwoMovement: Movements) {
+        const winningMovements: Record<Movements, Movements> = {
+            [Movements.SCISSORS]: Movements.PAPER,
+            [Movements.PAPER]: Movements.ROCK,
+            [Movements.ROCK]: Movements.SCISSORS,
+        }
 
-    if (playerOneMovement === Movements.SCISSORS && playerTwoMovement === Movements.PAPER) {
-      return 'Player_1'
-    }
+        if ( winningMovements[playerTwoMovement] === playerOneMovement) {
+            return 'Player_2'
+        }
+        if (playerOneMovement === Movements.PAPER && playerTwoMovement !== Movements.SCISSORS) {
+            return 'Player_1'
+        }
 
-    if (playerTwoMovement === Movements.ROCK) {
-      return 'Player_2'
-    }
-    if (playerTwoMovement === Movements.PAPER) {
-      return 'Player_2'
-    }
+        if (playerOneMovement === Movements.SCISSORS && playerTwoMovement === Movements.PAPER) {
+            return 'Player_1'
+        }
+
+        if (playerTwoMovement === Movements.ROCK) {
+            return 'Player_2'
+        }
+        if (playerTwoMovement === Movements.PAPER) {
+            return 'Player_2'
+        }
 
 
     return 'Player_1'
